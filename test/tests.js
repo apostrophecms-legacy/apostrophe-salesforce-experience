35f5397530106a897d1c3cf7f04378967aff0723
Alex Bea <alex.bea@gmail.com>
2019-05-29 16:21:50 -0400

    Test for user's experiences

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,11 @@
 const assert = require('assert');
 const fs = require('fs');
+const _ = require('lodash');
 
 describe('Salesforce Experience module', function () {
 
   let apos;
+  const testUserId = '0052E00000J72IP';
 
   this.timeout(25000);
 
@@ -23,8 +25,28 @@ describe('Salesforce Experience module', function () {
             secret: 'xyz'
           }
         },
-        'apostrophe-salesforce-experience': {},
-        'apostrophe-salesforce-connect': {},
+        'apostrophe-salesforce-experience': {
+          // Test are written using a real developer instance of Salesforce.
+          // adminUsername and adminPasswordAndToken are in an uncommitted
+          // `data/local.js` file for testing. Reach out to us in one of the
+          // Apostrophe support channels if you need them.
+          // https://apostrophecms.org/support
+          // (It's a dev Salesforce instance, but better safe than sorry.)
+          adminLoginUrl: 'https://punkave-dev-ed.my.salesforce.com/',
+          experienceQueries: [
+            {
+              soql: 'SELECT Name, Id FROM CollaborationGroup',
+              labelField: 'Name',
+              idField: 'Id',
+              userSoql: 'SELECT Name, Id FROM CollaborationGroup WHERE Id IN (SELECT CollaborationGroupId FROM CollaborationGroupMember WHERE MemberId = :AccountId)',
+              userLabelField: 'Name',
+              userIdField: 'Id'
+            }
+          ]
+        },
+        'apostrophe-salesforce-connect': {
+          test: true
+        },
         'apostrophe-salesforce-connect-widgets': {}
       },
       afterInit: function (callback) {
@@ -33,7 +55,6 @@ describe('Salesforce Experience module', function () {
         const sfcw = apos.modules['apostrophe-salesforce-connect-widgets'];
 
         assert(sfe.__meta.name === 'apostrophe-salesforce-experience');
-        assert(fs.existsSync(`${__dirname}/public/saml-metadata.xml`));
         assert(sfc.__meta.name === 'apostrophe-salesforce-connect');
         assert(sfcw.__meta.name === 'apostrophe-salesforce-connect-widgets');
 
@@ -46,12 +67,32 @@ describe('Salesforce Experience module', function () {
     });
   });
 
-  it('should have the basic experiences', async function () {
+  it('should create metadata', function (done) {
+    assert(fs.existsSync(`${__dirname}/public/saml-metadata.xml`));
+    done();
+  });
+
+  it('should have the basic and returned experiences', async function () {
     const sfe = apos.modules['apostrophe-salesforce-experience'];
     const experiences = await sfe.getExperienceChoices();
+    const returnedExp = await sfe.getExperiences();
 
-    assert(experiences.length === 2);
+    assert(experiences.length === 5);
     assert(experiences[0].label === 'Universal');
     assert(experiences[1].label === 'No Experience');
+    assert(Array.isArray(returnedExp) && returnedExp.length === 3);
+  });
+
+  it('should get the test user\'s experiences', async function () {
+    const sfe = apos.modules['apostrophe-salesforce-experience'];
+    const userExp = await sfe.getExperiences(testUserId);
+
+    assert(userExp.length === 2);
+    assert((_.find(userExp, {
+      label: 'Group One'
+    })).value === '0F92E000000EDiiSAG');
+    assert((_.find(userExp, {
+      label: 'Group Third'
+    })).value === '0F92E000000EDrDSAW');
   });
 });
